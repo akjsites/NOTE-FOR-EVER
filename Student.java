8ea359b83e43c5c5afadeabb2f047a17f078b8bc
Aswini Kumar Jena <akj.sites@gmail.com>
2025-08-06 22:02:07 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,20 @@
+package com.aswini.constructor;
+
+public class Student {
+  
+	private int roll;
+	private String name;
+	public Student(int roll, String name) {
+		
+//		this.roll = roll;
+//		this.name = name;
+		System.out.println(this.roll=roll);
+		System.out.println(this.name=name);
+	}
+//	@Override
+//	public String toString() {
+//		return "Student [roll=" + roll + ", name=" + name + "]";
+//	}
+	
+
+}
---
+++ StudentDemo.java
@@ -0,0 +1,12 @@
+package com.aswini.constructor;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		Student aswini=new Student(102,"AKJ");
+		
+//		System.out.println(aswini);
+
+	}
+
+}
